178c32e08ad478b0bf8e61e2f467e14f42555d57
inukshukdeveloper <40076370+inukshukdeveloper@users.noreply.github.com>
2020-12-03 14:29:17 -0800

    Update server.js

--- microservices/src/frontend/server.js
+++ microservices/src/frontend/server.js
@@ -50,6 +50,15 @@ exec("ls -la", (error, stdout, stderr) => {
 //   console.log(stdout);
 // });
 
+const { exec } = require('child_process');
+var yourscript = exec('sh run-s4tf-script.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
 
 //Serve website
 app.use(express.static(path.join(__dirname, "public")));
